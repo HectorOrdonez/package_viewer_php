f008b8ddcd91de46bc0bf9d1b2763a8455b1d434
Hector Ordonez <hjavierog@gmail.com>
2020-08-10 19:44:15 +0200

    Refactored PackageParser

--- src/Package/Parser/PackageParser.php
+++ src/Package/Parser/PackageParser.php
@@ -15,57 +15,77 @@ class PackageParser
      * It assumes the package exists in it
      *
      * @param array $source
-     * @param string $package
+     * @param $packageName
      * @return array
      */
-    public function parse($source, $package): array
+    public function parse($source, $packageName): array
     {
-        $fileFound = false;
-        $data = ['dependencies' => []];
+        $packageFound = false;
+        $data = [
+            'name' => $packageName,
+            'dependencies' => []
+        ];
 
         // First we need to find where the package info starts
         foreach ($source as $record) {
             if ($record[0] == 'Package') {
-                // File was already found, we are now facing another package
-                if ($fileFound == true) {
+                // Package was already found, we are now facing another package
+                if ($packageFound == true) {
                     return $data;
                 }
 
-                $packageName = trim($record[1]);
-
-                if ($packageName == $package) {
+                if ($packageName == trim($record[1])) {
                     // This is the package we are looking for!
-                    $fileFound = true;
-                    $data['name'] = $packageName;
+                    $packageFound = true;
+                    continue;
                 }
             }
 
-            if ($fileFound == true) {
-                // We do not need information related to multiple lines
-                if (count($record) == 1) {
-                    continue;
-                }
-
-                if ($record[0] == 'Depends') {
-                    $rawDependencies = explode(', ', trim($record[1]));
-                    $dependencies = [];
-
-                    foreach ($rawDependencies as $dependency) {
-                        list($dependencyName) = explode(' ', $dependency);
-
-                        $dependencies[] = [
-                            'name' => $dependencyName,
-                        ];
-                    }
-
-                    $data['dependencies'] = $dependencies;
-                } else if ($record[0] == 'Description') {
-                    $data['description'] = trim($record[1]);
-                }
+            if ($packageFound == true) {
+                $this->addData($data, $record);
             }
         }
 
-        // End of file
+        // End of package data
         return $data;
+    }
+
+    private function addData(array &$data, $record): void
+    {
+        // We do not need information related to multiple lines
+        if (count($record) == 1) {
+            return;
+        }
+
+        switch ($record[0]) {
+            case 'Depends':
+                $data['dependencies'] = $this->parseDependencies($record);
+                break;
+            case 'Description':
+                $data['description'] = trim($record[1]);
+                break;
+            default:
+                // We do not need this data
+                return;
+        }
+    }
+
+    /**
+     * @param $record
+     * @return array
+     */
+    private function parseDependencies($record): array
+    {
+        $rawDependencies = explode(', ', trim($record[1]));
+        $dependencies = [];
+
+        foreach ($rawDependencies as $dependency) {
+            list($dependencyName) = explode(' ', $dependency);
+
+            $dependencies[] = [
+                'name' => $dependencyName,
+            ];
+        }
+        return $dependencies;
     }
 }
